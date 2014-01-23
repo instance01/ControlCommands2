0dcdc0e78f51bc7d69f7a5229b4f018c54da295c
InstanceLabs <danielratke97@hotmail.de>
2014-01-23 21:53:48 +0100

    adds commands history; adds system wide hotkey registration

--- ControlCommands2/Form1.Designer.cs
+++ ControlCommands2/Form1.Designer.cs
@@ -29,16 +29,32 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
             this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(329, 26);
             this.textBox1.TabIndex = 0;
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Black;
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(329, 2);
+            this.listBox1.TabIndex = 1;
             // 
             // Form1
             // 
@@ -46,15 +62,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.BackColor = System.Drawing.Color.Blue;
-            this.ClientSize = new System.Drawing.Size(353, 55);
+            this.ClientSize = new System.Drawing.Size(353, 190);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.Opacity = 0D;
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "CommandControls";
             this.TransparencyKey = System.Drawing.Color.Blue;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -64,6 +83,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- ControlCommands2/Form1.cs
+++ ControlCommands2/Form1.cs
@@ -14,6 +14,8 @@ namespace ControlCommands2
     // TODO: add a console design
 
     // TODO: make custom commands available through config file
+    // example: [cmd] : [batch command]
+    // the [batch command] will be saved into a batch file, unfortunately only win batch commands available though
 
     // TODO: create a few commands
     // - processclean
@@ -25,17 +27,45 @@ namespace ControlCommands2
     // - [open web urls]
     // - google
     // - [something with fav music]
+    // - screenshot
 
     public partial class Form1 : Form
     {
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
         public Form1()
         {
             InitializeComponent();
+
+            int id = 0;
+            RegisterHotKey(this.Handle, id, 2, Keys.B.GetHashCode());
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == 0x0312)
+            {
+                if (this.Opacity < 0.1)
+                {
+                    this.Opacity = 1;
+                }
+                else
+                {
+                    this.Opacity = 0;
+                }
+                
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Location = new Point(10, 10);
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width / 2 - this.Width / 2, 10);
         }
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
@@ -43,16 +73,38 @@ namespace ControlCommands2
             if (e.KeyCode == Keys.Enter)
             {
                 String command = textBox1.Text;
+
+                // is false if no default command could be executed
+                bool success = true;
+
                 switch (command)
                 {
-                    case "":
-
-                        return;
+                    case "exit":
+                        Application.Exit();
+                        break;
                     default:
                         // TODO: open process
-                        return;
+
+                        success = false;
+                        break;
                 }
+
+                // update listbox
+                if (success)
+                {
+                    listBox1.Items.Add(command);
+                    if (listBox1.Items.Count < 11)
+                    {
+                        listBox1.Height = listBox1.ItemHeight * (listBox1.Items.Count + 1);
+                    }
+                }
+                return;
             }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, 0);
         }
     }
 }
